a839552652d842e4356948af8dd089d3e03bc96a
Pavel Balaev <mail@void.so>
2024-01-31 17:43:08 +0300

    add exceptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
+#![feature(abi_x86_interrupt)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buf;
 
@@ -23,6 +25,10 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
 
 pub trait Testable {
@@ -60,6 +66,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(abi_x86_interrupt)]
 #![feature(custom_test_frameworks)]
 #![test_runner(rosix::test_runner)]
 #![reexport_test_harness_main = "test_main"]
